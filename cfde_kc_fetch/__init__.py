6eeee2c11fb7364b74c41e742c89bd5a97701cd1
Zeyu Yao <cytronicoder@gmail.com>
2026-02-07 09:21:11 +0800

    chore: project metadata and license

---
+++ cfde_kc_fetch/__init__.py
@@ -0,0 +1,25 @@
+"""
+CFDE Knowledge Center Dataset Fetcher
+
+A Python package for downloading single-cell datasets and querying gene expression
+from the CFDE (Common Fund Data Ecosystem) Knowledge Center API.
+
+For documentation, see: https://cfde.hugeampkpnbi.org/docs
+"""
+
+__version__ = "0.1.0"
+__author__ = "Zeyu Yao"
+
+from .client import CFDEClient
+from .single_cell import (
+    download_dataset_registry,
+    download_single_cell_assets,
+    fetch_single_cell_lognorm,
+)
+
+__all__ = [
+    "CFDEClient",
+    "download_dataset_registry",
+    "download_single_cell_assets",
+    "fetch_single_cell_lognorm",
+]
